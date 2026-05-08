819951312f38ffe7e26a453af280ef022aae6195
Richmasters <richard@yourdigitalmasters.com>
2026-05-08 15:59:09 +0100

    Change index location

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: '/sort-my-life-out/app-dist/',
+  base: '/',
 })
